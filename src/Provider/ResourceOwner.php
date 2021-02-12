6e970fa55bc7a16e07773570ef645e38a4ba0c2e
Mehdi Bounya <mehdi.bounya@gmail.com>
2021-02-12 12:29:05 +0100

    Create ResourceOwner

---
+++ src/Provider/ResourceOwner.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace Mehdibo\OAuth2\Client\Provider;
+
+
+use League\OAuth2\Client\Provider\GenericResourceOwner;
+
+final class ResourceOwner extends GenericResourceOwner
+{
+
+    public function getEmail(): ?string
+    {
+        return $this->response['email'] ?? null;
+    }
+
+    public function getLogin(): ?string
+    {
+        return $this->response['login'] ?? null;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->response['first_name'] ?? null;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->response['last_name'] ?? null;
+    }
+
+    public function getImageUrl(): ?string
+    {
+        return $this->response['image_url'] ?? null;
+    }
+
+    public function getIsStaff(): ?bool
+    {
+        return $this->response['staff?'] ?? null;
+    }
+}
